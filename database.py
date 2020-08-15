9c87b05e04fb8c197beb567a447e2ef70f76d597
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-08-15 20:03:26 +0900

    .

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+import sqlite3
+from os.path import isfile
+
 class link:
     def __init__(self):
         self.uri = ""
@@ -6,7 +9,29 @@ class link:
 
 class DB:
     def __init__(self):
-        self.db = {}
+        self.is_already_exist = isfile("./uriDB.db")
+        self.db = sqlite3.connect("./uriDB.db")
+        self.cursor = self.db.cursor()
 
-    def insert(self, uri, short_uri):
+        print(self.is_already_exist)
+        if not self.is_already_exist:
+            self.cursor.execute('create table db(num int, URI text, SHORTURI text)')
+
+        self.size = self.size = len(self.cursor.fetchall())
+        print(self.size)
+
+    def insert(self):
+        self.cursor.execute(f'insert into db values({self.size+1}, ".", ".")')
+        self.db.commit()
+        return self.size+1
+
+    def update(self, num, uri, shorturi):
+        self.cursor.execute(f'update db set URI=? where num=?', (uri, num))
+        self.cursor.execute(f'update db set SHORTURI=? where num=?', (shorturi, num))
+        self.db.commit()
+
+    def select(self, short_uri):
         pass
+
+    def close(self):
+        self.db.close()
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from simplifier import base62
 from database import DB
 
+words = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+         'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+         'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+db = DB()
+
+
+def simplifier(num):
+    result = ""
+
+    while num % 62 > 0 or result == "":
+        result += words[num % 62]
+        num = int(num / 62)
+
+    return result
+
+
+def generate(uri):
+    num = db.insert()
+    shorturi = simplifier(num)
+    db.update(num, uri, shorturi)
+
+
+def get_request(res):
+    try:
+        data = db.select(res)
+    except:
+        pass
+
+
 if __name__ == "__main__":
-    db = DB()
-
     uri = "https://somekindofsomething.lol"
-    shorturi = base62()
-
-    print(shorturi)
+    generate(uri)
+    db.close()
