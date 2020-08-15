bb5919cda669b0a077e2c0c22d1fabe5ac504739
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-08-15 18:47:25 +0900

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+from simplifier import base62
+
+
+if __name__ == "__main__":
+    uri = ""
+    shorturi = base62(uri)
+
+    print(shorturi)
---
+++ simplifier.py
@@ -0,0 +1,20 @@
+def base62(index):
+    result = ""
+    words = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    while index % 62 > 0 or result == "":
+        result += words[index % 62]
+        index = index // 62
+
+        return result
+
+
+def generate(uri):
+    index = DB.insert(uri)
+    short_uri = base62(index)
+
+    DB.update(index, short_uri)
+
+    return short_uri
